43685cf81610c50b5493364bbb5cecaf2a5f6b70
SamuelSV26 <125204393+SamuelSV26@users.noreply.github.com>
2025-10-19 18:39:56 -0500

    footer presteza

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from '../share/navbar/navbar.component';
+import { HomeComponent } from '../home/home.component';
+import { FooterComponent } from '../share/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent, HomeComponent,FooterComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'project';
+
 }
